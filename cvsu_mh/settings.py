dfea9f2ef307dda02cf03947b6f760d44fe6e975
AjloriaCodex <ajloria123@gmail.com>
2024-09-13 19:53:35 +0800

    add vercel

--- cvsu_mh/settings.py
+++ cvsu_mh/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
