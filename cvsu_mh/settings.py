1161929f9615c6b083798f908d2bf9d29c6de0d3
AjloriaCodex <ajloria123@gmail.com>
2024-09-16 18:53:55 +0800

    readded whitenoise, change sql to postgresql

--- cvsu_mh/settings.py
+++ cvsu_mh/settings.py
@@ -79,6 +79,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,14 +112,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cvsu_mh.wsgi.application'
 
-
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cvsu_mh_data_base',  # Database name from Render
+        'USER': 'cvsu_mh_data_base_user',      # User from Render
+        'PASSWORD': 'vZxaPqZzHqS2mg7eMs1wJpIgnf8BZHPl',  # Password from Render
+        'HOST': 'dpg-crk0eklds78s73eg2a3g-a.singapore-postgres.render.com',      # Host from Render
+        'PORT': '5432',             # Default port for PostgreSQL
     }
 }
 
@@ -160,6 +165,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type    
