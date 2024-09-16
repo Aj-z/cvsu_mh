e81ab1b45dd80cf7fe95ff7c8391ac8049e8c739
AjloriaCodex <ajloria123@gmail.com>
2024-09-16 20:30:37 +0800

    uvicorn

--- cvsu_mh/wsgi.py
+++ cvsu_mh/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cvsu_mh.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
 
