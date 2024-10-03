58d33424e3ca1c3c9de73dea76d9e1d4f3e8aaeb
AjloriaCodex <ajloria123@gmail.com>
2024-10-03 22:37:47 +0800

    added multiple question fix names on cspear

--- cvsuquiz/forms.py
+++ cvsuquiz/forms.py
@@ -2,15 +2,27 @@ from django import forms
 from .models import AnswerEasyCspear
 
 class QuizForm(forms.Form):
-    CHOICES = [
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
+    CHOICES_Q1 = [
+        ('1', 'A. The branch of mechanics that studies the motion of a body or a system of bodies without consideration given to its mass or the forces acting on it.'),
+        ('2', 'B. The branch of mechanics that is concerned with the effects of forces on the motion of a body or system of bodies, especially of forces that do not originate within the system itself.'),
+        ('3', 'C. The study of the motion of a body or a system of bodies with consideration given to its mass or the forces acting on it.'),
+        ('4', 'D. If the forces acting upon an object are balanced, then the acceleration of that object will be 0 m/s/s.'),
+    ]
+    CHOICES_Q2 = [
+        ('1', 'A. Competence.'),
+        ('2', 'B. Relatedness.'),
+        ('3', 'C. Autonomy.'),
+        ('4', 'D. Intrinsic Motivation.'),
     ]
     
-    answer = forms.ChoiceField(
-        choices=CHOICES,
+    answer_q1 = forms.ChoiceField(
+        choices=CHOICES_Q1,
         widget=forms.RadioSelect,
-        label="What is 1 + 1?"
+        label="Whats is the definiton of Kinematics?"
+    )
+
+    answer_q2 = forms.ChoiceField(
+        choices=CHOICES_Q2,
+        widget=forms.RadioSelect,
+        label="Which of the following constructs within the Self-Determination Theory (SDT) is primarily focused on the psychological need for autonomy and the desire to make choices that align with one's interests and values?"
     )
