9cb9e4f5870f9f23a0050f8b78bed0ad85b32003
AjloriaCodex <ajloria123@gmail.com>
2024-10-04 23:09:13 +0800

    added question testnig cspear pic

--- cvsuquiz/forms.py
+++ cvsuquiz/forms.py
@@ -14,6 +14,12 @@ class QuizForm(forms.Form):
         ('3', 'C. Autonomy.'),
         ('4', 'D. Intrinsic Motivation.'),
     ]
+    CHOICES_Q3 = [
+        ('1', 'A. Ergogenic aids alone are sufficient to improve athletic performance, with no need for nutritional support.'),
+        ('2', 'B. Proper nutrition supports exercise recovery, but ergogenic aids can provide a significant boost in endurance, strength, and recovery beyond what nutrition alone can offer.'),
+        ('3', 'C. Proper nutrition and hydration are the most important factors for athletic performance, and ergogenic aids are only useful for professional athletes in extreme conditions.'),
+        ('4', 'D.  Ergogenic aids and proper nutrition complement each other; both can improve performance, but should be tailored to the individuals sport, body composition, and energy requirements.'),
+    ]
     
     answer_q1 = forms.ChoiceField(
         choices=CHOICES_Q1,
@@ -25,4 +31,9 @@ class QuizForm(forms.Form):
         choices=CHOICES_Q2,
         widget=forms.RadioSelect,
         label="Which of the following constructs within the Self-Determination Theory (SDT) is primarily focused on the psychological need for autonomy and the desire to make choices that align with one's interests and values?"
+    )
+    answer_q3 = forms.ChoiceField(
+        choices=CHOICES_Q3,
+        widget=forms.RadioSelect,
+        label=" Which of the following statements best describes the role of ergogenic aids and proper nutrition in enhancing athletic performance?"
     )
--- cvsuquiz/views.py
+++ cvsuquiz/views.py
@@ -7,12 +7,15 @@ def quiz_home_view(request):
 def quiz_home(request):
     feedback_q1 = None
     feedback_q2 = None
+    feedback_q3 = None
+
 
     if request.method == "POST":
         form = QuizForm(request.POST)
         if form.is_valid():
             answer_q1 = form.cleaned_data['answer_q1']
             answer_q2 = form.cleaned_data['answer_q2']
+            answer_q3 = form.cleaned_data['answer_q3']
 
             if answer_q1 == '4':
                 feedback_q1 = "Correct!"
@@ -23,11 +26,17 @@ def quiz_home(request):
                 feedback_q2 = "Correct!"
             else:
                 feedback_q2 = "Wrong! The correct answer is C"
+
+            if answer_q3 == 'C':
+                feedback_q3 = "Correct!"
+            else:
+                feedback_q3 = "Wrong! The correct answer is D"    
     else:
         form = QuizForm()
 
     return render(request, 'cvsuquiz/cspear.html', {
         'form': form, 
         'feedback_q1': feedback_q1,
-        'feedback_q2': feedback_q2
+        'feedback_q2': feedback_q2,
+        'feedback_q3': feedback_q3,
     })
