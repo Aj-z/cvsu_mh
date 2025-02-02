1b55810e844b28dd4f908a16fea6ae8a18faae37
AjloriaCodex <ajloria123@gmail.com>
2025-02-02 23:57:17 +0800

    update

--- cvsuquiz/urls.py
+++ cvsuquiz/urls.py
@@ -4,7 +4,7 @@ from .views import (cspear_home_view, BSESS_Quiz, BPED_Quiz,
                     BAE_Quiz, BSFT_Quiz, ced_home_view, 
                     BSE_ENGL_Quiz, BSE_FILI_Quiz, 
                     BSE_MATH_Quiz, BSE_SCIE_Quiz, 
-                    BSE_SS_Quiz,con_home_view, BSMT_QUIZ, BSM_QUIZ, BSN_QUIZ,
+                    BSE_SS_Quiz,BEED_Quiz,BSHM_Quiz,BSTM_Quiz,con_home_view, BSMT_QUIZ, BSM_QUIZ, BSN_QUIZ,
                     ceit_home_view,  
                     BSABE_QUIZ, BSARCH_QUIZ, BSCE_QUIZ, BSCpE_QUIZ, 
                     BSCS_QUIZ, BSEE_QUIZ, BSINDT_AT_QUIZ, BSINDT_ET_QUIZ,
@@ -36,6 +36,11 @@ urlpatterns = [
    path('BSE-MATH/', BSE_MATH_Quiz, name='BSE-MATH_quiz'),
    path('BSE-SCIE/', BSE_SCIE_Quiz, name='BSE-SCIE_quiz'),
    path('BSE-SS/', BSE_SS_Quiz, name='BSE-SS_quiz'),
+   path('BEED/', BEED_Quiz, name='BEED_quiz'),
+   path('BSHM/', BSHM_Quiz, name='BSHM_quiz'),
+   path('BSTM/', BSTM_Quiz, name='BSTM_quiz'),
+
+   # ------  CCJ  ---------
    path('CCJ/', ccj_home_view, name='CCJ_home'),
    # ------  CAS  ---------
    path('CAS/', cas_home_view, name='CAS_home'),
